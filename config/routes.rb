62b01252ce555488354a7997bbda4555b4586a3a
Hoersten <choersten@prodivnet.com>
2014-11-18 07:33:03 -0600

    Added User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :cities
   resources :counties
   resources :states
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117235446) do
+ActiveRecord::Schema.define(version: 20141118133038) do
 
   create_table "cities", force: true do |t|
     t.integer  "state_id"
@@ -75,5 +75,19 @@ ActiveRecord::Schema.define(version: 20141117235446) do
   end
 
   add_index "states", ["slug"], name: "index_states_on_slug", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "email",              default: "", null: false
+    t.string   "encrypted_password", default: "", null: false
+    t.integer  "sign_in_count",      default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
